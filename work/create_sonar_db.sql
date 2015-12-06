e692b97003c6f3e01beb1bd8d44c96ba7812a34a
Kazumasa Hagihara <hagihara.k@gmail.com>
2015-12-06 13:04:56 +0900

    環境構築用のスクリプト

---
+++ work/create_sonar_db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE sonar CHARACTER SET utf8;
+
+CREATE USER 'sonar' IDENTIFIED BY 'sonar';
+GRANT ALL ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar';
+GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar';
+FLUSH PRIVILEGES;
